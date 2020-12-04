243c3982852d91aafb7d845e55bb7b11224c451b
corentin1706 <corentin.latimier@ubtm.fr>
2020-12-04 22:18:21 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,10 @@
 #include <Arduino.h>
-#include <MFRC522.h>
-
-int led = 2;
+#include <MFRC522.h> //librairie pour gérer la communication avec la carte
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(led,OUTPUT);
 }
 
 void loop() {
-
-  for (int i=0;i<256;i++){
-    analogWrite(led,i);
-    Serial.println(i);
-  }
-
-  for(int i=255;i>0;i--){
-     analogWrite(led,i);
-      Serial.println(i);
-  }
-  
 }
